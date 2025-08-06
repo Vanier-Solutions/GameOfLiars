f58d286fc1528f7ce39de1dd48e9486f83a7be51
Tegvir <tmultani@students.cs.ubc.ca>
2025-08-05 17:43:49 -0700

    Implement initial lobby and player models with basic routes for creating and joining lobbies; outline future improvements for lobby code validation and auto-deletion.

--- server/services/lobbyService.js
+++ server/services/lobbyService.js
@@ -1,2 +1,4 @@
 // import { Lobby } from '../models/Lobby.js';
 
+// @Game-Of-Liars/ 
+// Okay so ive made the lobby and player models, as well as the 3 main lobby routes to create and join a lobby, as well as controller, I just need to work on services. I need to make sure when a user creates a lobby it doesnt use a code that is already in use and also I want the pregame lobby to end and delete in 5 minutes if nobody starts it. How do I go about this.
